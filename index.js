3955ac73e5f384e4ecde38b0ba527f552498c127
John Elliott <johnelliott703@gmail.com>
2016-11-11 15:17:34 -0500

    Enable CLI

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-module.exports = function tDays () {
+function tDays () {
   var election = new Date(2016, 10, 8)
   var now = new Date()
   return Math.floor((now - election) / (1000 * 60 * 60 * 24))
 }
 
+if (require.main === module) {
+  console.log(tDays())
+}
+
+module.exports = tDays
