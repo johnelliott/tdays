b85d3d879aafe76f21f40660b4819fa6453e4f80
John Elliott <johnelliott703@gmail.com>
2016-11-11 15:16:39 -0500

    Less Date manipulation

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 module.exports = function tDays () {
   var election = new Date(2016, 10, 8)
   var now = new Date()
-  var today = new Date(now.getFullYear(), now.getMonth(), now.getDate())
-  var delta = today - election
-  return delta / 1000 / 60 / 60 / 24
+  return Math.floor((now - election) / (1000 * 60 * 60 * 24))
 }
 
